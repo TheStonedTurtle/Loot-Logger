d3a9f858cec53c8c6584cfe75ac473d8bc57d418
TheStonedTurtle <mgoodwin559@gmail.com>
2019-12-20 05:18:29 -0800

    Add plugin config

---
+++ src/main/java/thestonedturtle/lootlogger/ItemSortTypes.java
@@ -0,0 +1,22 @@
+package thestonedturtle.lootlogger;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ItemSortTypes
+{
+	ALPHABETICAL("Alphabetical"),
+	ITEM_ID("Item ID"),
+	VALUE("Value"),
+	PRICE("Price");
+
+	private final String name;
+
+	@Override
+	public String toString()
+	{
+		return name;
+	}
+}
--- src/main/java/thestonedturtle/lootlogger/LootLoggerConfig.java
+++ src/main/java/thestonedturtle/lootlogger/LootLoggerConfig.java
@@ -8,12 +8,46 @@ import net.runelite.client.config.ConfigItem;
 public interface LootLoggerConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+		position = 0,
+		keyName = "uniquesPlacement",
+		name = "Uniques Placement",
+		description = "Where should unique items be displayed inside the UI"
 	)
-	default String greeting()
+	default UniqueItemPlacement uniquesPlacement()
 	{
-		return "Hello";
+		return UniqueItemPlacement.BOTH;
+	}
+
+	@ConfigItem(
+		position = 1,
+		keyName = "itemSortType",
+		name = "Item Sorting",
+		description = "Determines how items should be sorted inside the item breakdown"
+	)
+	default ItemSortTypes itemSortType()
+	{
+		return ItemSortTypes.VALUE;
+	}
+
+	@ConfigItem(
+		position = 2,
+		keyName = "itemBreakdown",
+		name = "Breakdown individual items",
+		description = "Shows more information for each item received instead of displaying the items in a compact grid"
+	)
+	default boolean itemBreakdown()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		position = 3,
+		keyName = "bossButtons",
+		name = "Show boss icons",
+		description = "Toggles whether the selection screen will use the boss icons"
+	)
+	default boolean bossButtons()
+	{
+		return true;
 	}
 }
